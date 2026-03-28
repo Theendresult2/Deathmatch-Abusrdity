cebc54e3c82778499e0f074de9eae44367335c8c
Theendresult2 <jomes624@gmail.com>
2026-03-28 17:09:21 -0500

    added gm dir

---
+++ deathmatchabsurdity/gamemode/shared.lua
@@ -0,0 +1,8 @@
+GM.Name = "Deathmatch Absurdity"
+GM.Author = "theendresult2"
+GM.Email = "shhhh"
+GM.Website = "https://github.com/Theendresult2/Deathmatch-Abusrdity"
+
+function GM:Initialize()
+	-- Do stuff
+end
